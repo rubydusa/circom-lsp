b0520ac9e09b8212d6bd4fcac30ee7d2ddcd8fca
rubydusa <rubydusa@protonmail.com>
2023-06-02 18:00:22 +0300

    feat(server): server version command

--- server/circom-lsp/src/main.rs
+++ server/circom-lsp/src/main.rs
@@ -1,15 +1,20 @@
 mod ast;
 mod backend;
+mod cli;
 mod constants;
 mod lsp_types_util;
 mod parse;
 mod wrappers;
 
 use backend::Backend;
+use clap::Parser;
+use cli::ServerOptions;
 use tower_lsp::{LspService, Server};
 
 #[tokio::main]
 async fn main() {
+    let _server_options = ServerOptions::parse();
+
     let stdin = tokio::io::stdin();
     let stdout = tokio::io::stdout();
 
